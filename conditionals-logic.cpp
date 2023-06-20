4454faeffab2b1af1b83d95e3294b06d86d53009
La <90055383+paulagrata@users.noreply.github.com>
2023-06-20 11:22:56 -0500

    boop.

--- conditionals-logic.cpp
+++ conditionals-logic.cpp
@@ -13,23 +13,17 @@ Logical operators
 
 int main() {
   
-  // Create a number that's 0 or 1
-  
+  // create a number that's 0 or 1
   srand (time(NULL));
-  int	coin = rand() % 2;
+  int coin = rand() % 2;
   
-  // If number is 0: Heads
-  // If it is not 0: Tails
-  
+  // if number is 0: heads
+  // if it is not 0: tails
   if (coin == 0) {
-  
     std::cout << "Heads\n";
-  
   }
 	else {
-	
     std::cout << "Tails\n";
-  
+		
   }
-  
 }
