c5a9ae99859861ce3e01ed767a785ec49b778981
La <90055383+paulagrata@users.noreply.github.com>
2023-06-21 16:31:10 -0500

    boop

--- loops.cpp
+++ loops.cpp
@@ -1,5 +1,26 @@
 /* loops:
 
+while
+for
 
 */
 // ______________________________________________________________
+
+//  program that asks and checks for a password - uses a while loop to ask the user for the password over and over again.
+int main() {
+  int pin = 0;
+  int tries = 0;
+  std::cout << "BANK OF LALA\n";
+  std::cout << "Enter your PIN: ";
+  std::cin >> pin;
+  tries++;
+  while (pin != 1234 && tries < 3) {
+    std::cout << "Enter your PIN: ";
+    std::cin >> pin;
+    tries++;
+  }
+  if (pin == 1234) {
+    std::cout << "PIN accepted!\n";
+    std::cout << "You now have access.\n"; 
+  }
+}
