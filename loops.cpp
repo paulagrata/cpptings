6f03b50d5bc6ce8fbe35d580af61ecfbcb2ae38f
La <90055383+paulagrata@users.noreply.github.com>
2023-07-18 15:53:35 -0500

    boop

--- loops.cpp
+++ loops.cpp
@@ -4,8 +4,8 @@ while
 for
 
 */
-// ______________________________________________________________
 
+// ______________________________________________________________
 //  program that asks and checks for a password - uses a while loop to ask the user for the password over and over again.
 int main() {
   int pin = 0;
@@ -23,4 +23,27 @@ int main() {
     std::cout << "PIN accepted!\n";
     std::cout << "You now have access.\n"; 
   }
+}
+
+// ______________________________________________________________
+// program that asks the user to guess a number between 1-10 and the answer is 8 with while loop so that they can guess up to 50 more times
+int main() {
+ 
+  int guess;
+  int tries = 0;
+ 
+  std::cout << "I have a number 1-10.\n";
+  std::cout << "Please guess it: ";
+  std::cin >> guess;
+ 
+  while (guess != 8 && tries < 50) {
+    std::cout << "Wrong guess, try again: ";
+    std::cin >> guess;
+    tries++;
+  }  
+ 
+  if (guess == 8) {
+    std::cout << "You got it!\n";
+  }  
+ 
 }
