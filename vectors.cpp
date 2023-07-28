92553df45ffdc4b3bf6b931152df49190d423d8b
La <90055383+paulagrata@users.noreply.github.com>
2023-07-28 16:16:10 -0500

    BOOP.

--- vectors.cpp
+++ vectors.cpp
@@ -27,14 +27,59 @@ get size/number of elements in vector:
 */
 
 // ______________________________________________________________
-//  main structure of a program:
+//  main structure of a program with vectors:
 
 #include <iostream>
 #include <vector>
 
 int main() {
-   
   std::vector<double> subway_adult;
   std::vector<double> subway_child;
-  
 }
+
+// ______________________________________________________________
+//  add costs and print total
+  
+int main() {
+  
+  std::vector<double> delivery_order;
+  
+  delivery_order.push_back(8.99);
+  delivery_order.push_back(3.75);
+  delivery_order.push_back(0.99);
+  delivery_order.push_back(5.99);
+  
+  double total = 0.0;
+  for (int i = 0; i < delivery_order.size(); i++) {
+  total  = delivery_order[i] + total ;
+  }
+  std::cout << total << "\n";
+}
+
+// ______________________________________________________________
+//  program to find the sum of even numbers and the product of odd numbers in a vector
+
+int main() {
+ 
+  int total_even = 0;
+  int product_odd = 1;
+ 
+  std::vector<int> vector = {2, 4, 3, 6, 1, 9};
+ 
+  for (int i = 0; i < vector.size(); i++) {
+    if (vector[i] % 2 == 0) {
+      total_even = total_even + vector[i];
+    } else {
+      product_odd = product_odd * vector[i];
+    }
+  }
+   
+  std::cout << "sum of even: " << total_even << "\n";
+  std::cout << "product of odd: " << product_odd;
+   
+  }
+
+// ______________________________________________________________
+//  
+  
+ 
