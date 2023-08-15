da94e530e33e9c545f14dc11b15540fe26e9b523
La <90055383+paulagrata@users.noreply.github.com>
2023-08-15 12:27:43 -0500

    booop.

---
+++ classes-objects.cpp
@@ -0,0 +1,16 @@
+/* 
+
+classes/objects
+
+classes - serves as a blueprint for objects, which are instances of the class [just like age is an instance of int - int age]
+
+components of a class are called class members [you can access class members using the dot operator - object.class_member]
+two types of class members:
+- attributes [also known as member data] consist of information about an instance of the class.
+- methods [also known as member functions] are functions that you can use with an instance of the class
+  [use a . before method names to distinguish them from regular functions]
+
+*/
+
+// ______________________________________________________________
+//  main structure of a program:
