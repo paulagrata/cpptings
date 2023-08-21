d6ed5284c23f499342582ee03ce5d7e40f2d3f1a
La <90055383+paulagrata@users.noreply.github.com>
2023-08-21 12:54:07 -0500

    updates.

--- classes-objects.cpp
+++ classes-objects.cpp
@@ -3,6 +3,8 @@
 classes/objects
 
 classes - serves as a blueprint for objects, which are instances of the class [just like age is an instance of int - int age]
+- empty classes are useless
+- must declare a method inside the class if we want to define it outside
 
 components of a class are called class members [you can access class members using the dot operator - object.class_member]
 two types of class members:
@@ -10,7 +12,23 @@ two types of class members:
 - methods [also known as member functions] are functions that you can use with an instance of the class
   [use a . before method names to distinguish them from regular functions]
 
+object - an instance of a class that encapsulates data and functionality pertaining to that data
+create (or instantiate) an object:
+City nameofcity;
+set object’s attributes values:
+nameofcity.population = 123456789;
+using a method created in a class [see below example]
+nameofcity.get_population();
 */
 
 // ______________________________________________________________
-//  main structure of a program:
+//  main structure of a class with attribute and method:
+class City {
+  // attribute
+  int population;
+public:
+  // method
+  void add_resident() {
+    population++;
+  }
+};
