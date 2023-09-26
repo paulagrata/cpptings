6eca5746212708699d8430e161526b19521a30b9
La <90055383+paulagrata@users.noreply.github.com>
2023-09-26 16:02:09 -0500

    updated.

--- classes-objects.cpp
+++ classes-objects.cpp
@@ -158,3 +158,55 @@ std::string Song::get_title() {
 std::string Song::get_artist() {
   return artist;
 }
+
+// script to display user profile:
+// app.cpp
+#include <iostream>
+#include "profile.hpp"
+int main() {
+  Profile sam("Sam Drakkila", 30, "New York", "USA", "he/him");
+  sam.add_hobby("listening to audiobooks and podcasts");
+  sam.add_hobby("playing rec sports like bowling and kickball");
+  sam.add_hobby("writing a speculative fiction novel");
+  sam.add_hobby("reading advice columns");
+  std::cout << sam.view_profile();
+}
+// profile.cpp
+#include <iostream>
+#include "profile.hpp"
+Profile::Profile(std::string new_name, int new_age, std::string new_city, std::string new_country, std::string new_pronouns)
+  : name(new_name), age(new_age), city(new_city), country(new_country), pronouns(new_pronouns) {
+  if (new_age >= 18) {
+    age = new_age;
+  } else {
+    age = 0;
+  }
+}
+std::string Profile::view_profile() {
+  std::string bio = "Name: " + name;
+  bio += "\nAge: " + std::to_string(age);
+  bio += "\nPronouns: " + pronouns;
+  std::string hobby_string = "Hobbies:\n";
+  for (std::string hobby : hobbies) {
+    hobby_string += " - " + hobby + "\n";
+  }
+  return bio + "\n" + hobby_string;
+}
+void Profile::add_hobby(std::string new_hobby) {
+  hobbies.push_back(new_hobby);
+}
+// profile.hpp
+#include <vector>
+class Profile {
+private:
+  std::string name;
+  int age;
+  std::string city;
+  std::string country;
+  std::string pronouns;
+  std::vector<std::string> hobbies;
+public:
+  Profile(std::string new_name, int new_age, std::string new_city, std::string new_country, std::string new_pronouns = "they/them");
+  std::string view_profile();
+  void add_hobby(std::string new_hobby);
+}; 
