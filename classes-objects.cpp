fb46803ac872e002f1e334421d845e0352be8d78
La <90055383+paulagrata@users.noreply.github.com>
2023-09-26 16:09:46 -0500

    updated.

--- classes-objects.cpp
+++ classes-objects.cpp
@@ -164,12 +164,12 @@ std::string Song::get_artist() {
 #include <iostream>
 #include "profile.hpp"
 int main() {
-  Profile sam("Sam Drakkila", 30, "New York", "USA", "he/him");
-  sam.add_hobby("listening to audiobooks and podcasts");
-  sam.add_hobby("playing rec sports like bowling and kickball");
-  sam.add_hobby("writing a speculative fiction novel");
-  sam.add_hobby("reading advice columns");
-  std::cout << sam.view_profile();
+  Profile bella("Bella Goth", 31, "Pleasantview", "Casanova", "she/her");
+  bella.add_hobby("practicing organ");
+  bella.add_hobby("loves golfing");
+  bella.add_hobby("gardening exotic plants");
+  bella.add_hobby("disappering randomly");
+  std::cout << bella.view_profile();
 }
 // profile.cpp
 #include <iostream>
