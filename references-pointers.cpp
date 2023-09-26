95f028cbf0edfe3a1253be8898cedda62f1f05b4
La <90055383+paulagrata@users.noreply.github.com>
2023-09-26 15:36:16 -0500

    boop.

--- references-pointers.cpp
+++ references-pointers.cpp
@@ -1,10 +1,24 @@
 /* 
 
 references/pointers
-[powerful features in C++; allow programmers to directly manipulate memory – the most critical and scarce resource in computer – in order to optimize performance]
+[powerful features in C++; allow programmers to directly manipulate memory in order to optimize performance]
 
-references - 
+references - reference variable is an alias for something else [another name for an already existing variable]
+- it is created using the & sign
+- anything done to the reference also happens to the original.
+- aliases cannot be changed to alias something else
 
 pointers - 
 
 */
+
+// ______________________________________________________________
+//  main structure of a reference:
+#include <iostream>
+int main() {
+  int soda = 99;
+  // setting pop as a reference to soda
+  int &pop = soda;
+  pop++;
+  std::cout<<soda<<"\n"<<pop<<"\n";
+}
